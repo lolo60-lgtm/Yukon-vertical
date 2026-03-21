a6e3db16c2451775cf85d776b71be12d8d38048f
lolo60-lgtm <lolo.60@mail.ru>
2026-03-21 15:22:29 +0100

    Update lead-button.tsx

--- components/lead-button.tsx
+++ components/lead-button.tsx
@@ -1,1 +1,20 @@
+"use client"
 
+import { openLeadModal } from "@/components/lead-modal"
+
+interface LeadButtonProps {
+  title?: string
+  children: React.ReactNode
+  className?: string
+}
+
+export function LeadButton({ title, children, className }: LeadButtonProps) {
+  return (
+    <button
+      onClick={() => openLeadModal(title)}
+      className={className}
+    >
+      {children}
+    </button>
+  )
+}
