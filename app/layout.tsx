288005a6759dea1d6b8da669e1925d4ccb736d75
lolo60-lgtm <lolo.60@mail.ru>
2026-03-25 16:52:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,26 +43,34 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ru">
-      <body
-        className={`${delaGothicOne.variable} ${playfairDisplay.variable} ${montserrat.variable} font-sans antialiased`}
-      >
-       {/* Facebook Pixel */}
-    <Script id="fb-pixel" strategy="afterInteractive">
-      {`
-        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
-        (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1234033088686995');
-        fbq('track', 'PageView');
-      `}
-    </Script>
+  <html lang="ru">
+    <body className={`${delaGothicOne.variable} ${playfairDisplay.variable} ${montserrat.variable} font-sans antialiased`}>
+      
+      {/* Facebook Pixel */}
+      <Script id="fb-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+          (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1292910256020888'); 
+          fbq('track', 'PageView');
+        `}
+      </Script>
 
-    {children}
-    <Analytics />
-  </body>
-</html>
-)
+      {/* Вставляем noscript сразу после Script или в начало body */}
+      <noscript>
+        <img 
+          height="1" 
+          width="1" 
+          style={{ display: 'none' }} 
+          src="https://www.facebook.com/tr?id=1292910256020888&ev=PageView&noscript=1" 
+        />
+      </noscript>
+
+      {children}
+    </body>
+  </html>
+);
 }
