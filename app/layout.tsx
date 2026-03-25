dcb3e19669c982ad0524ebe430fe068998b8732a
lolo60-lgtm <lolo.60@mail.ru>
2026-03-25 20:47:29 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
           n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
           t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
           (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1292910256020888'); 
+          fbq('init', '1292910256020888', { autoConfig: false });
           fbq('track', 'PageView');
         `}
       </Script>
