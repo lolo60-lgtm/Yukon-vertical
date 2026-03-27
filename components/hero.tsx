76b438ad18038ce7c41f283752aa070ed7823674
lolo60-lgtm <lolo.60@mail.ru>
2026-03-27 22:42:24 +0100

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -30,7 +30,7 @@ export function Hero() {
 
       <div
         ref={ref}
-        className="relative z-10 mx-auto max-w-5xl translate-y-8 px-4 py-4 text-center opacity-0 transition-all duration-1000 ease-out"
+        className="relative z-10 mx-auto max-w-5xl translate-y-8 px-4 py-12 text-center opacity-0 transition-all duration-1000 ease-out"
       >
         <h1 className="text-balance font-mono text-4xl font-bold leading-tight tracking-tight text-primary-foreground sm:text-5xl md:text-6xl lg:text-7xl">
           {"Быстрый Код "}
